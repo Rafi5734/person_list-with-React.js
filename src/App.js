f42c85e4bd1fb1910373b4fac2d3aff5252c292b
Kamrul Hasan Rafi <75684189+Rafi5734@users.noreply.github.com>
2021-06-04 21:52:19 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './person/person';
+import Another from './another';
+
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > hello world < /h1>    <
+        p > this not properly working < /p>  <
+        Person / >
+
+        <
+        Another / >
+        <
+        /
+        div >
+
+
+    );
+}
+
+export default App;
---
+++ src/another.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const another = () => {
+    return <p> this is another </p>;
+};
+export default another;
